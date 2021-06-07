b12f53dc3270e9e5865813fba5a374db2260b85c
Love AVL <51767480+Spartak-Belov-Floresku@users.noreply.github.com>
2021-06-07 08:01:21 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SelectField, IntegerField
 from wtforms.validators import InputRequired, Optional, NumberRange, URL
 
 class CupcakeForm(FlaskForm):
-    """generate form to add pet into db"""
+    """generate form"""
     rate = [(i, f'Rating {i}') for i in range(1,11)]
     flavor = StringField('Flavor', validators=[InputRequired()])
     size = SelectField('Size', choices=[('large', 'Large'), ('medium', 'Medium'), ('small', 'Small')])
